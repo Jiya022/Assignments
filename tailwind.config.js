45f1880d687fa68401ade837ae5bd46113e9135a
Jiya Choudhary <jiyac3729@gmail.com>
2025-11-11 19:14:14 +0530

    completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./Assignment-52.1/index.html', 'Assignment-52.2/index.html'],
+  content: ['./Assignment-52.1/index.html', 'Assignment-52.2/index.html', 'Assignment-52.3/index.html'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
